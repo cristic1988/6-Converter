c2f3275b57d1cb50a80821383201243b5944febb
cristic1988 <cristic8@gmail.com>
2024-05-13 21:58:07 +0300

    Add tempF to tempC convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,20 @@ package app;
 
 public class Main {
 
+    private static final double CONV_K1 = 1.8;
+    private static final double CONV_K2 = 32;
+
     public static void main(String[] args) {
         System.out.println("App for converting units of temperature.");
+        double tempF = 12;
+        double celsiyDegree = convtempFTotempC(tempF);
+        System.out.println("Result is " + celsiyDegree + " tempC.");
 
     }
+
+    private static double convtempFTotempC(double tempF) {
+        return (tempF - CONV_K2) / CONV_K1;
+
+    }
+
 }
